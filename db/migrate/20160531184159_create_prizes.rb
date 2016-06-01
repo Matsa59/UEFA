cd9ba6649c7c5ac76c24a6f5d3111218bd7db501
quentin <quentin.buirette@hotmail.fr>
2016-06-01 17:46:14 +0200

    Add two field scores in Matches table, and few modification in back office

--- db/migrate/20160531184159_create_prizes.rb
+++ db/migrate/20160531184159_create_prizes.rb
@@ -2,7 +2,7 @@ class CreatePrizes < ActiveRecord::Migration
   def change
     create_table :prizes do |t|
       t.string :name, :limit => 150
-      t.string :description, :limit => 255
+      t.text :description
       t.integer :quantity
       t.string :ref, :limit => 80
     end
